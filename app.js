bdba2bfbcc61d1cc4e7c2e906304e031dc2cf7cb
unknown <gurumitkalpramod@gmail.com>
2025-03-11 18:13:51 +0530

    js file changed

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const getProducts = async () => {
     <a href="/products/index.html?id=${product.id}">
     <div class="border-[1px] border-gray-400 p-4 rounded-[12px] h-[330px] hover:scale-105 duration-300 cursor-pointer">
     <div>
-    <img src="${product.image}" class="h-[200px] w-[300px] flex flex-col justify-center items-center" alt="" />
+    <img src="${product.image}" class="h-[200px] w-[300px] " alt="" />
     </div>
     <div class="p-3">
     <h3 class="font-semibold">${product.title.length > 40 ? product.title.slice(0,40) + "..." : product.title}</h3>
